812a5f1c2f7921a234ec784882300fabbee0b7b8
Emerson Dias de Oliveira <emersondiaspd@gmail.com>
2021-09-05 20:56:03 -0300

    Ajustando API do pedido

--- src/main/java/com/fiap/persistencia/resources/PedidoResource.java
+++ src/main/java/com/fiap/persistencia/resources/PedidoResource.java
@@ -2,24 +2,19 @@ package com.fiap.persistencia.resources;
 
 import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.fiap.persistencia.domain.Pais;
 import com.fiap.persistencia.domain.Pedido;
-import com.fiap.persistencia.domain.dto.PaisDTO;
 import com.fiap.persistencia.services.PedidoService;
 
 import io.swagger.annotations.Api;
@@ -48,7 +43,7 @@ public class PedidoResource {
 	}
 
 	@ApiOperation(value = "Buscar os pedidos do cliente", tags = { "Pedidos" })
-	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/{idCliente}", method = RequestMethod.GET)
 	public ResponseEntity<List<Pedido>> findByCliente(@PathVariable Integer idCliente) {
 		List<Pedido> list = pedidoService.findByCliente(idCliente);
 		return ResponseEntity.ok().body(list);
