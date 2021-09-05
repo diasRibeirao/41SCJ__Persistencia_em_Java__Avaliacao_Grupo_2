0da3c530e26229200ca5cdf30172853c074e0e08
Emerson Dias de Oliveira <emersondiaspd@gmail.com>
2021-09-05 15:18:16 -0300

    Ajustes resource endereço

--- src/main/java/com/fiap/persistencia/resources/EnderecoResource.java
+++ src/main/java/com/fiap/persistencia/resources/EnderecoResource.java
@@ -16,47 +16,54 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.fiap.persistencia.domain.Endereco;
 import com.fiap.persistencia.domain.Pais;
+import com.fiap.persistencia.domain.dto.EnderecoDTO;
 import com.fiap.persistencia.domain.dto.PaisDTO;
+import com.fiap.persistencia.services.EnderecoService;
+
+import io.swagger.annotations.Api;
 
 @RestController
+@Api(value = "Endereços", description = "APIs Endereço", tags = { "Endereços" })
 @RequestMapping(value = "/endereco")
 public class EnderecoResource {
 	@Autowired
 	private EnderecoService enderecoService;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<PaisDTO>> findAll() {
-		List<Pais> list = ederecoService.findAll();
-		List<PaisDTO> listDto = list.stream().map(obj -> new PaisDTO(obj)).collect(Collectors.toList());
+	public ResponseEntity<List<EnderecoDTO>> findAll() {
+		List<Endereco> list = enderecoService.findAll();
+		List<EnderecoDTO> listDto = list.stream().map(obj -> new EnderecoDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<Pais> find(@PathVariable Integer id) {
-		Pais obj = ederecoService.find(id);
-		return ResponseEntity.ok().body(obj);
+	public ResponseEntity<Endereco> find(@PathVariable Integer id) {
+		Endereco endereco = enderecoService.find(id);
+		return ResponseEntity.ok().body(endereco);
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody PaisDTO objDto) {
-		Pais obj = ederecoService.fromDTO(objDto);
-		obj = ederecoService.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+	public ResponseEntity<Void> insert(@Valid @RequestBody EnderecoDTO enderecoDTO) {
+		Endereco endereco = enderecoService.fromDTO(enderecoDTO);
+		endereco = enderecoService.insert(endereco);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(endereco.getId())
+				.toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@Valid @RequestBody PaisDTO objDto, @PathVariable Integer id) {
-		Pais obj = ederecoService.fromDTO(objDto);
-		obj.setId(id);
-		obj = ederecoService.update(obj);
+	public ResponseEntity<Void> update(@Valid @RequestBody EnderecoDTO enderecoDTO, @PathVariable Integer id) {
+		Endereco endereco = enderecoService.fromDTO(enderecoDTO);
+		endereco.setId(id);
+		endereco = enderecoService.update(endereco);
 		return ResponseEntity.noContent().build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
-		ederecoService.delete(id);
+		enderecoService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
 
