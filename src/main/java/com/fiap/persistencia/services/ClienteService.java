820fbb737d5e9efd4b7856ce78386076574ae7c4
Emerson Dias de Oliveira <emersondiaspd@gmail.com>
2021-09-05 13:35:46 -0300

    Configurado REDIS na service Cliente

--- src/main/java/com/fiap/persistencia/services/ClienteService.java
+++ src/main/java/com/fiap/persistencia/services/ClienteService.java
@@ -17,7 +17,6 @@ import com.fiap.persistencia.domain.Endereco;
 import com.fiap.persistencia.domain.dto.ClienteDTO;
 import com.fiap.persistencia.domain.dto.ClienteNewDTO;
 import com.fiap.persistencia.domain.enums.TipoCliente;
-import com.fiap.persistencia.repositories.CidadeRepository;
 import com.fiap.persistencia.repositories.ClienteRepository;
 import com.fiap.persistencia.services.exceptions.DataIntegrityException;
 import com.fiap.persistencia.services.exceptions.ObjectNotFoundException;
@@ -29,12 +28,12 @@ public class ClienteService {
 	private ClienteRepository clienteRepository;
 
 	@Autowired
-	private CidadeRepository cidadeRepository;
+	private CidadeService cidadeService;
 
 	@Cacheable(value= "clienteCache", key= "#id")
 	public Cliente find(Integer id) {
-		Optional<Cliente> obj = clienteRepository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		Optional<Cliente> clientes = clienteRepository.findById(id);
+		return clientes.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 
@@ -47,24 +46,26 @@ public class ClienteService {
 		 put= { @CachePut(value= "clienteCache", key= "#cliente.id") },
 		 evict= { @CacheEvict(value= "allClientesCache", allEntries= true) } 
 	)
-	public Cliente insert(Cliente obj) {
-		obj.setId(null);
-		return clienteRepository.save(obj);
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		return clienteRepository.save(cliente);
 	}
 
 	@Caching(
 		 put= { @CachePut(value= "clienteCache", key= "#cliente.id") },
 		 evict= { @CacheEvict(value= "allClientesCache", allEntries= true) }
 	)
-	public Cliente update(Cliente obj) {
-		Cliente newObj = find(obj.getId());
-		updateData(newObj, obj);
+	public Cliente update(Cliente cliente) {
+		Cliente newObj = find(cliente.getId());
+		updateData(newObj, cliente);
 		return clienteRepository.save(newObj);
 	}
 
 	@Caching(
-		 put= { @CachePut(value= "clienteCache", key= "#cliente.id") },
-		 evict= { @CacheEvict(value= "allClientesCache", allEntries= true) }
+		evict= { 
+			@CacheEvict(value= "clienteCache", key= "#id"),
+			@CacheEvict(value= "allClientesCache", allEntries= true)
+		}
 	)
 	public void delete(Integer id) {
 		find(id);
@@ -75,12 +76,6 @@ public class ClienteService {
 		}
 	}
 
-	private Cidade findCidadeById(Integer cidade_Id) {
-		Optional<Cidade> obj = cidadeRepository.findById(cidade_Id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + cidade_Id + ", Tipo: " + Cidade.class.getName()));
-	}
-
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), null, objDto.getNome(), objDto.getEmail(), null);
 	}
@@ -88,7 +83,7 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteNewDTO objDto) {
 		Cliente cli = new Cliente(null, objDto.getCpfOuCnpj(), objDto.getNome(), objDto.getEmail(),
 				TipoCliente.toEnum(objDto.getTipo()));
-		Cidade cid = findCidadeById(objDto.getCidadeId());
+		Cidade cid = cidadeService.find(objDto.getCidadeId());
 		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(),
 				objDto.getBairro(), objDto.getCep(), objDto.getPrincipal(), cli, cid);
 		cli.getEnderecos().add(end);
